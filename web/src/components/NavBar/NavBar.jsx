8a514a38d1a65c6220d8704e57a3e24a54801dec
Dants0 <gui.dg1@hotmail.com>
2023-01-09 21:44:27 -0300

    (front): add navbar

--- web/src/components/NavBar/NavBar.jsx
+++ web/src/components/NavBar/NavBar.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Link } from "react-router-dom";
 
+
 import styles from './styles.module.scss'
+
 
 const NavBar = () => {
     return (
-        <header>
+        <header className={styles.container}>
             <h1>EstudY</h1>
-            <input type="search" placeholder='Pesquise um tema' />
+            <input type="search" placeholder='Pesquise um tema' /> 
+    
             <nav>
                 <ul>
                     <li>
